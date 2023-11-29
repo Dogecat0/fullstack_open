733bf0fca1a3622bb8d0b0109a4544fc2b1a73c5
Dogecat0 <zcmmwang@gmail.com>
2023-11-29 18:22:17 +0000

    part2 exercise2 phonebook

---
+++ part2/phonebook/src/components/Person.jsx
@@ -0,0 +1,13 @@
+const Person = ({ personsToShow }) => {
+  return (
+    <div>
+        {personsToShow.map(person => 
+            <div key={person.id}>
+            {person.name} {person.number}
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Person
